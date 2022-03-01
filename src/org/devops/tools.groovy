511623be86678064ce9728c9ae8f9c5c7097805f
L1an <1048315883@qq.com>
2022-03-01 22:55:30 +0800

    optimization plugin

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -2,15 +2,16 @@ package org.devops
 
 // 打印内容
 def PrintMsg(content) {
-    PrintMsg(content, 'blue')
+    PrintMsg(content, 'black')
 }
 
 def PrintMsg(content, color) {
     colors = [
-        'red'   : "\003[40;31m >>>>>>>>>>${content}<<<<<<<<<< \033[0m",
-        'blue'  : "\003[47;34m ${content} \033[0m",
-        'green' : "·[1:32m >>>>>>>>>>${content}<<<<<<<<<<·[m",
-        'green2' : "\033[40;32m >>>>>>>>>>${content}<<<<<<<<<< \033[0m",
+        'black'  : "\033[30m ${content} \033[0m",
+        'red'    : "\033[31m >>>>>${content}<<<<< \033[0m",
+        'green'  : "\033[32m >>>>>${content}<<<<< \033[0m",
+        'yellow' : "\033[33m >>>>>${content}<<<<< \033[0m",
+        'blue'   : "\033[34m >>>>>${content}<<<<< \033[0m",
         ]
     ansiColor('xterm') {
         println(colors[color])
